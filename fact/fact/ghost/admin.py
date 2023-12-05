f032ce1741b4f7625c05716a164e89518fd4e4e3
krasorx <you@example.com>
2023-12-04 22:06:38 -0300

    ahora como arreglo esto

---
+++ fact/fact/ghost/admin.py
@@ -0,0 +1,8 @@
+from tokenize import group
+from unicodedata import category
+from django.contrib import admin
+from django.utils.http import urlencode
+from fact.ghost import models
+
+
+admin.site.site_header = "Luisesp Panel de control"
